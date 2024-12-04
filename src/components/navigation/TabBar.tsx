775f78e4a3809a979b8d976070cdfaf9c989d5a8
condua <phuc.phanphuc2002@hcmut.edu.vn>
2024-12-04 18:46:37 +0700

    updated

--- src/components/navigation/TabBar.tsx
+++ src/components/navigation/TabBar.tsx
@@ -37,7 +37,15 @@ function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
   const owner = wallet?.currentData?.owner;
   const walletType = wallet?.currentData?.type;
   const { bottom } = useSafeAreaInsets();
-
+  if (!wallet?.currentData) {
+    // Nếu ví không tồn tại
+    Alert.alert(
+      "Thông báo",
+      "Ví của bạn không còn tồn tại. Chuyển đến trang ví.",
+      [{ text: "OK", onPress: () => navigate("/wallet") }]
+    );
+    return false;
+  }
   const hideTabs = useMemo(() => {
     for (let route of state.routes) {
       const { options } = descriptors[route.key];
